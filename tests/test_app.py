c4da44c7df8802cd5679f779ecbcece0cfe5bb20
bomis1 <107684740+bomis1@users.noreply.github.com>
2022-08-04 07:34:55 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,6 @@
 # Import the necessary modules
-#from flask import url_for
-from flask import *
-#from flask_testing import TestCase
-from flask_testing import *
+from flask import url_for
+from flask_testing import TestCase
 
 # import the app's classes and objects
 from app import app, db, Team, Player 
